60e1be26dd73a32a6b7aba288cdbbe00364b9626
Evgeniy <evgeniyzinkov98@gmail.com>
2023-03-24 10:16:09 +0100

    add: navigation

--- crypto-project/src/App.js
+++ crypto-project/src/App.js
@@ -1,7 +1,24 @@
-import "./index.css"
+import { Route, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import CoinDetail from "./pages/CoinDetail"
+import Watchlist from "./pages/Watchlist"
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/coin/:id" element={<CoinDetail />} />
+          <Route path="/watchlist" element={<Watchlist />} />
+        </Routes>
+      </div>
+    </div>
+  )
 }
 
 export default App
