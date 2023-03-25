ecfc65d0305ee352b3800f148ced056956f0f401
Evgeniy <evgeniyzinkov98@gmail.com>
2023-03-25 16:12:49 +0100

    add: Header and CoinList component to home page

--- crypto-project/src/pages/Home.jsx
+++ crypto-project/src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react"
+import CoinList from "../components/CoinList"
+import Header from "../components/Header"
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <>
+      <Header />
+      <CoinList />
+    </>
+  )
 }
 
 export default Home
