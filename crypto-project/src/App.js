e7ddbd53fcf62573f10b361caf17388db680833c
Evgeniy <evgeniyzinkov98@gmail.com>
2023-03-25 16:11:37 +0100

    add: Footer component

--- crypto-project/src/App.js
+++ crypto-project/src/App.js
@@ -1,15 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
-import Header from "./components/Header"
 import Home from "./pages/Home"
 import CoinDetail from "./pages/CoinDetail"
 import Watchlist from "./pages/Watchlist"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <div className="app">
       <Navbar />
-      <Header />
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -17,6 +16,7 @@ function App() {
           <Route path="/watchlist" element={<Watchlist />} />
         </Routes>
       </div>
+      <Footer />
     </div>
   )
 }
