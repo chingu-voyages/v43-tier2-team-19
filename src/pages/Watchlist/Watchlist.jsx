b0b8e4f2efd470ffe38e8841965d19b61122824d
Evgeniy <evgeniyzinkov98@gmail.com>
2023-04-15 13:35:46 +0200

    feat: remove from watchlist

--- src/pages/Watchlist/Watchlist.jsx
+++ src/pages/Watchlist/Watchlist.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
-import { CoinLink, Container, Desc, Img, Name } from './Watchlist.styled';
+import React, { useState } from "react"
+import { CoinLink, Container, Desc, Img, Name } from "./Watchlist.styled"
 
 const Watchlist = () => {
   // retrieve watchlist data from local storage
-  const watchlist = JSON.parse(localStorage.getItem('watchlist')) || [];
-  console.log(watchlist);
+  const [watchlist, setWatchlist] = useState(
+    JSON.parse(localStorage.getItem("watchlist")) || []
+  )
+
+  const removeFromWatchlist = (id) => {
+    const updatedWatchlist = watchlist.filter((coin) => coin.id !== id)
+    setWatchlist(updatedWatchlist)
+    localStorage.setItem("watchlist", JSON.stringify(updatedWatchlist))
+  }
 
   return (
     <Container>
       {watchlist.map((coin) => (
-        <CoinLink to={`/coin/${coin.id}`}>
-          <Img src={coin.image.small} alt="img" />
-          <Name>{coin.name}</Name>
-          <Desc>{coin.market_data.current_price.usd}$</Desc>
-        </CoinLink>
+        <>
+          <CoinLink key={coin.id} to={`/coin/${coin.id}`}>
+            <Img src={coin.image.small} alt="img" />
+            <Name>{coin.name}</Name>
+            <Desc>{coin.market_data.current_price.usd}$</Desc>
+          </CoinLink>
+          <button onClick={() => removeFromWatchlist(coin.id)}>Remove</button>
+        </>
       ))}
     </Container>
-  );
-};
+  )
+}
 
-export default Watchlist;
+export default Watchlist
