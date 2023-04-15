6f5243fb5876e61b4995ab19e3c99b882d946505
Evgeniy <evgeniyzinkov98@gmail.com>
2023-04-15 13:40:49 +0200

    add: button styles

--- src/pages/Watchlist/Watchlist.styled.js
+++ src/pages/Watchlist/Watchlist.styled.js
@@ -1,16 +1,17 @@
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import styled from "styled-components"
+import { Link } from "react-router-dom"
 
 export const Container = styled.div`
   display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 
   @media (max-width: 480px) {
     flex-direction: column;
     align-items: center;
   }
-`;
+`
 
 export const CoinLink = styled(Link)`
   display: flex;
@@ -28,14 +29,14 @@ export const CoinLink = styled(Link)`
   @media (max-width: 768px) {
     margin-bottom: 1rem;
   }
-`;
+`
 
 export const Img = styled.img`
-  width: 100px;
-  height: 100px;
+  width: 60px;
+  height: 60px;
   object-fit: contain;
   margin-bottom: 1rem;
-`;
+`
 
 export const Name = styled.h3`
   font-size: 1.5rem;
@@ -46,7 +47,7 @@ export const Name = styled.h3`
     font-size: 1.2rem;
     margin-bottom: 0.25rem;
   }
-`;
+`
 
 export const Desc = styled.p`
   font-size: 1.2rem;
@@ -55,4 +56,25 @@ export const Desc = styled.p`
   @media (max-width: 768px) {
     font-size: 1rem;
   }
-`;
+`
+
+export const Button = styled.button`
+  margin-bottom: 20px;
+  font-size: 1.5rem;
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 0.25rem;
+  background-color: #01b0c8;
+  color: #fff;
+  cursor: pointer;
+  transition: background-color 350ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:hover {
+    background-color: #3792cb;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 1.2rem;
+    padding: 0.25rem 0.75rem;
+  }
+`
